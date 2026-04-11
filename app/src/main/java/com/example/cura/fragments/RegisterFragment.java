e366bdc9b8984001647514936fa1b1caed374116
davidexvii-uni <d.califano18@studenti.unisa.it>
2026-04-11 13:05:51 +0200

    Registrazione

--- app/src/main/java/com/example/cura/fragments/RegisterFragment.java
+++ app/src/main/java/com/example/cura/fragments/RegisterFragment.java
@@ -1,10 +1,18 @@
 package com.example.cura.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.cura.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,6 +28,42 @@ public class RegisterFragment extends Fragment {
         super.onCreate(savedInstanceState);
         // Inizializza l'istanza di Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        EditText emailEditText = view.findViewById(R.id.email);
+        EditText passwordEditText = view.findViewById(R.id.password);
+        EditText c_passwordEditText = view.findViewById(R.id.passwordconfirm);
+        CheckBox tosCheckBox = view.findViewById(R.id.checkbox);
+
+        Button btnRegister = view.findViewById(R.id.crea);
+        btnRegister.setOnClickListener(v -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            String confirmPassword = c_passwordEditText.getText().toString();
+
+
+            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(getContext(), "Compila tutti i campi", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (!password.equals(confirmPassword)) {
+                c_passwordEditText.setError("Le password non coincidono");
+                return;
+            }
+
+            if (!tosCheckBox.isChecked()) {
+                Toast.makeText(getContext(), "Accetta i termini di servizio", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            registraNuovoUtente(email, password);
+        });
+
+        return view;
     }
 
     private void registraNuovoUtente(String email, String password) {
